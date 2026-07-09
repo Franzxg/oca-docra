86827c0b51ecae896b3ea417a75dc8f0cb8fdea3
Franzxg <franzgronchi@gmail.com>
2026-07-09 18:18:09 +0200

    Start working on the Homepage

---
+++ src/HomeLogo.jsx
@@ -0,0 +1,11 @@
+import logo from "./imgs/logo-home.png";
+
+function HomeLogo() {
+  return (
+    <div>
+      <img src={logo} className="logo" alt="Logo" />
+    </div>
+  );
+}
+
+export default HomeLogo;
---
+++ src/HomeMenu.jsx
@@ -0,0 +1,12 @@
+function HomeMenu() {
+  return (
+    <div className="home-menu">
+      <a href="">Illustrazioni</a>
+      <a href="">Almanacco dell'oca</a>
+      <a href="">Contatti</a>
+      <a href="">Biografia</a>
+    </div>
+  );
+}
+
+export default HomeMenu;
