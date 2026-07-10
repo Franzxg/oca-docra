f04dbde5f4294d5d3c32d4c847dfd37dfe25b5b4
Franzxg <franzgronchi@gmail.com>
2026-07-10 15:12:53 +0200

    Footer written and added to Contacts page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        © 2026 Oca d'ocra Illustrazioni. All rights reserved. Reproduction and
+        use in AI datasets is not permitted.
+      </p>
+    </footer>
+  );
+}
--- src/routes/contatti.jsx
+++ src/routes/contatti.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function Contatti() {
   return (
     <>
       <Navbar></Navbar>
+      <Footer></Footer>
       {/* <h1>Contatti</h1> */}
     </>
   );
