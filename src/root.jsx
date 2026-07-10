43f8c842d1642e6094eeee64841566dc87ba75c8
Franzxg <franzgronchi@gmail.com>
2026-07-10 16:22:01 +0200

    Title head in root.jsx changed

--- src/root.jsx
+++ src/root.jsx
@@ -11,7 +11,7 @@ export function Layout({ children }) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="../public/favicon.ico" sizes="any" />
-        <title>oca-docra</title>
+        <title>Oca d'Ocra</title>
         <Meta />
         <Links />
       </head>
