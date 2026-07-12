00ed4fae79ca54f206cc31cace5fa528a9feee5e
Franzxg <franzgronchi@gmail.com>
2026-07-12 11:19:30 +0200

    Added all the other illustration projects with their cards

--- src/components/IllustrationsSection.jsx
+++ src/components/IllustrationsSection.jsx
@@ -1,5 +1,9 @@
 import ActionAreaCard from "./ActionAreaCard";
 import idromele from "../imgs/illustrazioni/Idromele/IMG_0874.JPG";
+import erbario from "../imgs/illustrazioni/Erbario/Copertina_erbario.JPG";
+import libro_illustrato from "../imgs/illustrazioni/Libro_illustrato/Copertina.jpg";
+import playing_cards from "../imgs/illustrazioni/Maestro_e_Margherita_Cards/IMG_1088.jpg";
+import mago from "../imgs/illustrazioni/Mago_di_Oz/IMG_1095.jpg";
 
 const illustrazioni = [
   {
@@ -8,8 +12,30 @@ const illustrazioni = [
     image: idromele,
     description: "Etichetta per idromele artigianale",
   },
-  { id: 2, title: "", image: "", description: "" },
-  { id: 3, title: "", image: "", description: "" },
+  {
+    id: 2,
+    title: "Erbario",
+    image: erbario,
+    description: "Erbario illustrato, progetto personale",
+  },
+  {
+    id: 3,
+    title: "Il signore delle maschere",
+    image: libro_illustrato,
+    description: "Libro illustrato",
+  },
+  {
+    id: 4,
+    title: "Il Maestro e Margherita",
+    image: playing_cards,
+    description: "Playing Cards ispirate al romanzo 'Il Maestro e Margherita'",
+  },
+  {
+    id: 5,
+    title: "Il Mago di Oz",
+    image: mago,
+    description: "Illustrazioni per il romanzo 'Il Mago di Oz'",
+  },
 ];
 export default function IllustrationsSection() {
   return (
