7d64e230866776df8d57d49e37f567a3570881a1
Franzxg <franzgronchi@gmail.com>
2026-07-13 11:50:15 +0200

    Definition of media query breakpoints

--- src/theme.js
+++ src/theme.js
@@ -4,6 +4,20 @@ const theme = createTheme({
   typography: {
     fontFamily: '"PT Serif", serif',
   },
+  palette: {
+    background: {
+      default: "#f2e8cf",
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
 });
 
 export default theme;
