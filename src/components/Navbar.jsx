e9b4f33e8dfdd3b8e47a88649a9ad63607f53644
Franzxg <franzgronchi@gmail.com>
2026-07-13 16:07:28 +0200

    Media query for Navbar added and finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,51 @@
+import { useState } from "react";
 import { Link } from "react-router";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 import logo from "../imgs/logo-home.png";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const links = [
+    { to: "/", label: "Home" },
+    { to: "/illustrazioni", label: "Illustrazioni" },
+    { to: "/racconti", label: "Almanacco dell'oca" },
+    { to: "/biografia", label: "Biografia" },
+    { to: "/contatti", label: "Contatti" },
+  ];
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <img src={logo} alt="Logo" className="logo-navbar" />
       </div>
+
       <div className="navbar-links">
-        <Link to="/">Home</Link>
-        <Link to="/illustrazioni">Illustrazioni</Link>
-        <Link to="/racconti">Almanacco dell'oca</Link>
-        <Link to="/biografia">Biografia</Link>
-        <Link to="/contatti">Contatti</Link>
+        {links.map((link) => (
+          <Link key={link.to} to={link.to}>
+            {link.label}
+          </Link>
+        ))}
+      </div>
+
+      <button
+        className="navbar-toggle"
+        onClick={() => setMenuOpen(!menuOpen)}
+        aria-label={menuOpen ? "Chiudi menu" : "Apri menu"}
+        aria-expanded={menuOpen}
+      >
+        {menuOpen ? <CloseIcon /> : <MenuIcon />}
+      </button>
+
+      <div
+        className={`navbar-mobile-menu ${menuOpen ? "navbar-mobile-menu-open" : ""}`}
+      >
+        {links.map((link) => (
+          <Link key={link.to} to={link.to} onClick={() => setMenuOpen(false)}>
+            {link.label}
+          </Link>
+        ))}
       </div>
     </nav>
   );
