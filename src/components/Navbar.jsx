18f375d799b116b47e23a331168cc5c9f1f41cb8
Franzxg <franzgronchi@gmail.com>
2026-07-13 12:54:30 +0200

    Start adjusting Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,18 @@
 import { Link } from "react-router";
-import Home from "../routes/home";
-import HomeLogo from "./HomeLogo";
 import logo from "../imgs/logo-home.png";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="div-navbar">
+      <div className="navbar-logo">
+        <img src={logo} alt="Logo" className="logo-navbar" />
+      </div>
+      <div className="navbar-links">
+        <Link to="/">Home</Link>
         <Link to="/illustrazioni">Illustrazioni</Link>
         <Link to="/racconti">Almanacco dell'oca</Link>
         <Link to="/biografia">Biografia</Link>
         <Link to="/contatti">Contatti</Link>
-      </div>
-      <div className="div-navbar">
-        <img src={logo} alt="Logo" className="logo-navbar" />
-      </div>
-      <div className="div-navbar">
-        <a
-          href="https://www.instagram.com/ocadocra/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Instagram
-        </a>
-        <a href="https://www.behance.net/sofiacasavecchi1">Behance</a>
-        <a href="https://ocadocra.substack.com">Substack</a>
-        <a href="https://ko-fi.com/ocadocra">Ko-fi</a>
       </div>
     </nav>
   );
